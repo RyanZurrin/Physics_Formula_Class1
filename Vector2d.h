6de83932e495663097b89ed019f1e0516ac59bed
Ryan Zurrin <rzurrin@gmail.com>
2021-05-01 11:29:57 -0400

    inlined the vector 2d and vector 3d classes

--- Vector2d.h
+++ Vector2d.h
@@ -1,10 +1,7 @@
 #pragma once
-//author:			Ryan Zurrin
-//file:           vect.h (XL)
-// Part of a final project in c++ II, this program as a whole
-// will calculate the properties of 2D and 3D objects and vectors
-
-// Specification file for a  vect class
+//author:	Ryan Zurrin
+//file:		Vector2d.h
+// Specification file for a  vector class
 #ifndef VECTOR2D_H
 #define VECTOR2D_H
 
@@ -12,6 +9,8 @@
 #include <iomanip>
 #include <cmath>
 #include <complex>
+#include <cassert>
+using namespace std;
 typedef  long double _ld_;
 using namespace std;
 
@@ -21,8 +20,6 @@ const _ld_ DEGREE = 180/_PI_;
 const _ld_ RADIAN = _PI_/180;
 const _ld_ _Ga_ = 9.8;
 static int object_counter = 0;
-
-
 
 class Vector
 {
@@ -142,6 +139,600 @@ public:
 	virtual ~Vector();
 };
 #endif
+//=============================================================================
+//in-line class Implementation
 
+inline Vector::Vector()
+{
+  _vptr2d =  nullptr;
+  x = 0;
+  y = 0;
+  magnitude = 0.0;
+  angle = 0.0;
+  calculate_arcLength();
+  arcLength = 0.0;
+  revolutionAngle_inDegrees = 0.0;
+  mode = 'r';
+  validate_setMode();
+
+  countIncrease();
+  //countShow();
+}
+
+inline Vector::Vector(char _mode)
+{
+	_vptr2d =  nullptr;
+  x = 0;
+  y = 0;
+  magnitude = 0;
+  angle = 0;
+  calculate_arcLength();
+  arcLength = return_angle();
+  revolutionAngle_inDegrees = 0;
+  mode = _mode;
+  validate_setMode();
+  countIncrease();
+  //countShow();
+}
+/*______________________________________________________________________________
+  the third argument is the mode setting.
+  Use 'r'/'R' for rectangular components, 'p'/'P' for polar components
+*/
+inline Vector::Vector(_ld_ xMag, _ld_ yAng, char _mode)
+{
+	revolutionAngle_inDegrees = 0;
+	magnitude = 0;
+	angle = 0;
+	mode = _mode;
+	validate_setMode();
+	x = xMag;
+	y = yAng;
+	set_coordinates(x, y, _mode);
+	calculate_arcLength();
+	arcLength = return_angle();
+	_vptr2d =  nullptr;
+	countIncrease();
+
+}
+inline Vector::Vector(const Vector &v)
+{
+
+
+	revolutionAngle_inDegrees = v.revolutionAngle_inDegrees;
+	magnitude = v.magnitude;
+	angle = v.angle;
+	mode = v.mode;
+	validate_setMode();
+	x = v.x;
+	y = v.y;
+	set_coordinates(x, y, mode);
+	calculate_arcLength();
+	arcLength = return_angle();
+	_vptr2d =  nullptr;
+	countIncrease();
+}
+inline void Vector::showAllData()const
+{
+
+  showRectCord();
+  showPolarCord();
+  showRevolutionAngle();
+ // show_mode();
+  show_arcLength();
+}
+inline void Vector::showVector()const
+{
+  if (mode == 'r' || mode == 'R') {
+   showRectCord();
+   show_arcLength();
+  }
+  else {
+   showPolarCord();
+   show_arcLength();
+  }
+}
+
+inline void Vector::showRectCord()const
+{
+	 cout << setprecision(5) << fixed;
+
+	 cout << "\n(x,y) = ";
+	 if(x < 0 && x > -1 ){
+		 cout << setiosflags(ios::fixed);
+	 cout << fixed << "(" << x << ","
+		 << resetiosflags(ios::fixed);
+   }
+	 else{
+	 cout << "(" << x << ",";
+   }
+   if(y<0 && y>-1){
+	   cout << setiosflags(ios::fixed);
+	   cout << y << ")" <<endl;
+		 cout << resetiosflags(ios::fixed);
+	 }
+	 else{
+		cout << y << ")";
+	 }
+}
+inline void Vector::showPolarCord()const
+{
+	cout << setprecision(5) << fixed;
+	 if(magnitude >= 0 && angle >= 0){
+		 cout << "\n<r,\xE9> = <" << magnitude << "," << angle * RADIAN << ">";
+	 }
+	 else{
+		 cout << setiosflags(ios::fixed);
+		 cout << "\n<r,\xE9> = <" << magnitude << "," << angle * RADIAN << ">";
+		 cout << resetiosflags(ios::fixed) << endl;
+	 }
+}
+inline void Vector::showRevolutionAngle()const
+{
+  cout << "Angle in degrees:  " << revolutionAngle_inDegrees;
+}
+inline void Vector::show_x() const
+{
+	cout << "\nx: " << x << endl;
+}
+inline void Vector::show_y() const
+{
+	cout << "\ny: " << y << endl;
+}
+inline void Vector::show_mag() const
+{
+	cout << "\nr: " << magnitude << endl;
+}
+inline void Vector::show_angle() const
+{
+	cout << "\n\xE9: " << angle << "\xF8" << endl;
+}
+inline void Vector::show_mode()const
+{
+	if (mode == 'p' || mode == 'P') {
+		cout << "\nmode: Polar" << endl;
+	}
+	else
+		cout << "\nmode: Rectangular" << endl;
+}
+inline void Vector::show_arcLength() const
+{
+   cout << setprecision(7)<< fixed;
+	cout << "\narc length: " << arcLength << endl;
+}
+inline _ld_ Vector::return_x() const
+{
+	return x;
+}
+inline _ld_ Vector::return_y()const
+{
+	return y;
+}
+inline _ld_ Vector::return_mag()const
+{
+	return magnitude;
+}
+inline _ld_ Vector::return_angle()const
+{
+	return angle;
+}
+inline _ld_ Vector::return_arcLength() const
+{
+	return arcLength;
+}
+inline char Vector::return_mode()const
+{
+  return mode;
+}
+/**
+ *method: set_coordinates(_ld_ xMag, _ld_ yAng, char _mode)
+* arguments: x, y mode = 'r' or 'p' .by default is 'r'ectangular, 'p'= polar.
+* Use 'r'/'R' for rectangular components, 'p'/'P' for polar components
+*/
+inline void Vector::set_coordinates(_ld_ xMag, _ld_ yAng, char _mode)
+{
+  validate_setMode();
+
+  if (tolower(_mode) ==  'r') {
+	set_rectCord(xMag, yAng);
+	adjust_angle();
+  }
+  else{
+	  set_polarCord(xMag, yAng);
+	  adjust_angle();
+  }
+  calculate_arcLength();
+}
+inline void Vector::set_rectCord(_ld_ _x, _ld_ _y)
+{
+  x = _x;
+  y = _y;
+  calculate_polar();
+  revolutionAngle_inDegrees = angle;
+}
+inline void Vector::set_polarCord(_ld_ mag, _ld_ ang)
+{
+  magnitude = mag;
+  normalize_magnitude();
+  angle =ang;
+  revolutionAngle_inDegrees = ang;
+  adjust_angle();
+  calculate_rectangular();
+}
+inline void Vector::set_x(_ld_ _x)
+{
+  x = _x;
+  calculate_polar();
+}
+inline void Vector::set_y(_ld_ _y)
+{
+  y = _y;
+  calculate_polar();
+}
+inline void Vector::set_mag(_ld_ mag)
+{
+  magnitude = mag;
+  normalize_magnitude();
+  calculate_rectangular();
+  calculate_arcLength();
+}
+inline void Vector::set_angle(_ld_ ang)
+{
+  angle = ang;
+  adjust_angle();
+  calculate_arcLength();
+  calculate_rectangular();
+}
+inline void Vector::set_mode(char _mode)
+{
+  mode = _mode;
+  validate_setMode();
+}
+inline _ld_ Vector::square()const
+{
+	return  x * x + y * y;
+}
+inline _ld_ Vector::dot_product(const Vector& vec)const
+{
+	return  x * vec.x + vec.y * y;
+}
+inline _ld_ Vector::distance(const Vector& vec)const
+{
+	_ld_ x1, x2, y1, y2, t1, t2;
+	x1 = this->x;
+	x2 = vec.x;
+	y1 = this->y;
+	y2 = vec.y;
+	t1 = x2 - x1;
+	t2 = y2 - y1;
+	return sqrt(pow(t1, 2) + pow(t2, 2));
+}
+inline _ld_ Vector::cross_product2D(const Vector& v)const
+{
+	return (x * v.y) - (y * v.x);
+}
+inline Vector Vector::normalization()
+{
+	assert(find_magnitude() != 0);  // NOLINT(clang-diagnostic-float-equal)
+	*this /= find_magnitude();
+	return *this;
+}
+inline _ld_ Vector::find_magnitude()const
+{
+	return sqrt(square());
+}
+/*
+void Vector::setPolarCurve()
+{
+	cout << "\nEnter the Polar Curve to convert to cartiesian equation\n>";
+	cin >> Curve.r;
+  Curve.x = sqrt(pow(Curve.r, 2) + pow(Curve.y, 2));
+  Curve.theta = atan(y / x);
+}
+*/
+inline void Vector::validate_setMode()
+{
+	do{
+		  if (tolower(mode) == 'r' || tolower(mode) == 'p') {
+	   break;
+		  }
+			else {
+		cout << "\nEnter a valid mode, r/R for rectangular\n"
+			 << "or p/P or polar components\n>";
+		cin >> mode;
+		cin.clear();
+		cin.ignore(100, '\n');
+			 }
+  }while(!cin || tolower(mode) != 'r' || tolower(mode) != 'p');
+}
+inline void Vector::normalize_magnitude()
+{
+  if (magnitude < 0)
+	magnitude = abs(magnitude);
+}
+inline void Vector::calculate_magnitude()
+{
+   magnitude = sqrt(x * x + y * y);
+}
+inline void Vector::calculate_angle()
+{
+	angle = atan2(y, x) * DEGREE;
+}
+inline void Vector::adjust_angle()
+{
+  if(angle == -360 || angle == -720 || angle == -1080 || angle == -1440){  // NOLINT(clang-diagnostic-float-equal)
+	angle = 0;
+  }
+	if (angle >= 360){
+		const int rev_degrees = static_cast<int>(angle) / 360;
+		const int to_subtract = rev_degrees *360;
+		const _ld_ new_angle = angle - to_subtract;
+		angle = new_angle;
+	}else if (angle < 0 && angle > -360){
+		angle += 360;
+		cout <<"test:" << angle << endl;
+	}else if(angle < -360){
+		const int rev_degrees = static_cast<int>(angle) /360;
+		const int to_subtract = rev_degrees *360;
+		const _ld_ newAngle = angle - to_subtract + 360;
+		angle = newAngle;
+	}
+}
+inline void Vector::calculate_rectangular()
+{
+  x = magnitude* cos(angle*RADIAN);
+  y = magnitude* sin(angle*RADIAN);
+}
+inline void Vector::calculate_polar()
+{
+	 magnitude = sqrt(x * x + y * y);
+	if (x == 0.0 && y == 0.0)
+		angle = 0.0;
+	else
+		angle = atan2(y, x)*DEGREE;
+
+	adjust_angle();
+	calculate_arcLength();
+	revolutionAngle_inDegrees = angle;
+}
+inline void Vector::calculate_arcLength()
+{
+  adjust_angle();
+  arcLength = (_PI_ * (magnitude*2)) *
+		(angle / 360.0);
+}
+inline Vector Vector::check_division(_ld_ d)
+{
+	return *this;
+}
+inline Vector Vector::check_division(Vector d)
+{
+	Vector temp;
+	return temp;
+}
+inline bool Vector::operator<(const Vector &v) const
+{
+  if (magnitude < v.magnitude)
+	return true;
+  return false;
+}
+inline bool Vector::operator<=(const Vector& v) const
+{
+	if (magnitude <= v.magnitude)
+		return true;
+	return false;
+}
+inline bool Vector::operator>(const Vector & v)const
+{
+  if (magnitude > v.magnitude)
+	return true;
+  return false;
+}
+inline bool Vector::operator>=(const Vector& v) const
+{
+	if (magnitude >= v.magnitude)
+		return true;
+
+	return false;  // NOLINT(clang-diagnostic-misleading-indentation)
+}
+inline bool Vector::operator==(const Vector & v)const
+{
+  if (magnitude == v.magnitude && angle == v.angle)  // NOLINT(clang-diagnostic-float-equal)
+	return true;
+  return false;
+}
+inline bool Vector::operator!=(const Vector& v) const
+{
+	if (!(magnitude == v.magnitude && angle == v.angle))  // NOLINT(clang-diagnostic-float-equal)
+		return true;
+	else
+		return false;
+}
+inline Vector Vector::operator+(const _ld_ n)const
+{
+	cout << "in  Vector::operator+(const _ld_ n)const"<<endl;
+	Vector total(x + n, y + n);
+	total.mode = mode;
+	return total;
+}
+inline Vector& Vector::operator+=(const Vector& v)
+{
+	cout << "in Vector& Vector::operator+=(const Vector& v)" << endl;
+	Vector sum;
+	sum.x += v.x;
+	sum.y += v.y;
+	sum.mode = v.mode;
+	return *this;
+}
+inline Vector Vector::operator+(const Vector& v)const
+{
+	cout << "in  Vector::operator+(const Vector& v)const"<<endl;
+  Vector sum(x + v.x, y + v.y);
+  sum.mode = mode;
+  return sum;
+}
+inline Vector Vector::operator+()const
+{
+	cout << "in  Vector::operator+()const"<<endl;
+	Vector total(x+x, y+y);
+	total.mode = mode;
+  return total;
+}
+inline Vector Vector::operator++()
+{
+	Vector sum(++x, ++y);
+	sum.mode = mode;
+  return sum;
+}
+inline Vector Vector::operator++(int)
+{
+  Vector sum(x++, y++, mode);
+  return sum;
+}
+inline Vector Vector::operator-(const Vector& v) const
+{
+  Vector sum(x-v.x, y-v.y, mode);
+  return sum;
+}
+inline Vector Vector::operator-(const _ld_ number)const
+{
+	Vector total(x - number, y - number);
+	total.mode = mode;
+	return total;
+}
+inline Vector Vector::operator-()const
+{
+	Vector total(-x, -y);
+	total.mode = mode;
+  return total;
+}
+inline Vector Vector::operator--()
+{
+  Vector difference(--x, --y);
+  difference.mode = mode;
+  return difference;
+}
+inline Vector Vector::operator--(int)
+{
+  Vector difference(x--, y--);
+  difference.mode = mode;
+  return difference;
+}
+inline Vector operator-(_ld_ s, Vector& v)
+{
+ Vector total(s-v.return_x(),s-v.return_y(),v.return_mode());
+ return total;
+}
+inline Vector operator+(_ld_ s, Vector& v)
+{
+ Vector total(s+v.return_x(),s+v.return_y(),v.return_mode());
+ return total;
+}
+inline Vector& Vector::operator=(const Vector &right)
+{
+  if (this != &right) {
+
+		x = right.x;
+		y = right.y;
+		calculate_polar();
+		calculate_arcLength();
+	}
+  return *this;
+}
+inline Vector& Vector::operator=(const Vector *vec)
+{
+  x = vec->x;
+  y = vec->y;
+  calculate_polar();
+  calculate_arcLength();
+  return *this;
+}
+inline Vector& Vector::operator=(Vector&& right)noexcept
+{
+	 if (this != &right)
+	 {
+		 swap(x, right.x);
+		 swap(y, right.y);
+		 this->calculate_polar();
+	 }
+	 return *this;
+}
+inline Vector::Vector(Vector&& temp) noexcept
+{
+	_vptr2d = nullptr;
+	x = temp.x;
+	y = temp.y;
+	mode = temp.mode;
+	calculate_polar();
+	calculate_arcLength();
+	revolutionAngle_inDegrees = return_angle();
+	angle = return_angle();
+	magnitude = return_mag();
+	arcLength = return_arcLength();
+	calculate_arcLength();
+}
+inline Vector Vector::operator/(_ld_ d)
+{
+	return check_division(d);
+}
+inline Vector Vector::operator/(int d)
+{
+	return check_division(d);
+}
+inline Vector Vector::operator/(Vector v)
+{
+	return check_division(v);
+}
+inline Vector &Vector::operator/=(_ld_ value)
+{
+	x /= value;
+	y /= value;
+	calculate_polar();
+	calculate_angle();
+	return *this;
+
+}
+inline Vector Vector::operator*(const _ld_ scalar)const
+{
+	Vector results(x*scalar, y*scalar);
+	results.mode = mode;
+	return results;
+}
+inline Vector operator*(_ld_ s, Vector& v)
+{
+  return v * s;
+}
+inline Vector operator*(Vector& v, Vector& s)
+{
+   Vector results(v.x*s.x, v.y*s.y, v.mode);
+   return results;
+}
+inline ostream& operator<<(ostream& os, const Vector& v)
+{
+  if(v.mode == 'R' || v.mode == 'r'){
+	v.showRectCord();
+  }
+  else{
+	v.showPolarCord();
+  }
+  return os;
+}
+inline istream& operator>>(istream& is, Vector& v)
+{
+  if(v.mode == 'R' || v.mode == 'r'){
+	is>> v.x >> v.y;
+	v.calculate_polar();
+  }
+  else{
+	is>> v.magnitude >> v.angle;
+	v.calculate_rectangular();
+  }
+  return is;
+}
+inline Vector::~Vector()
+{
+	delete _vptr2d;
+	countDecrease();
+	//countShow();
+}
 
 
--- Vector3D.h
+++ Vector3D.h
@@ -1,6 +1,6 @@
 // Part of a final project in c++ II, this program as a whole
 // will calculate the properties of 2D and 3D objects and vectors
-// prgrammer:   Ryan Zurrin
+// author:   Ryan Zurrin
 #ifndef VECTOR3D_H
 #define VECTOR3D_H
 #include "Vector2d.h"
@@ -14,55 +14,373 @@ class Vector3D : public Vector
 private:
 
 public:
-    _ld z, xAngle, yAngle, zAngle;
-    void set_allAngles();
-    void set_magnitude();
-    Vector3D check_division(_ld);
-    Vector3D* _ptr3d;
-    Vector3D(); //constructor
-    Vector3D(_ld, _ld, _ld);  //initializing object with values.
-    Vector3D(const Vector3D &vec);    //copy constructor
-    void setX(_ld);
-    void setY(_ld);
-    void setZ(_ld);
-    void set_xAngle(_ld);
-    void set_yAngle(_ld);
-    void set_zAngle(_ld);
-    void set_coordinates(_ld, _ld, _ld);
-    void showAllData();
-    void showPolarCord()const;
-    void display();    //display value of vectors
-    _ld returnX()const; //return x
-    _ld returnY()const; //return y
-    _ld returnZ()const; //return z
-    _ld return_xAngle()const; //return x angle
-    _ld return_yAngle()const; //return y angle
-    _ld return_zAngle()const; //return z angle
-    _ld square(); //gives square of the vector
-    _ld dot_product(const Vector3D &vec) const; //scalar dot_product
-    _ld distance(const Vector3D &vec);    //gives distance between two vectors
-    _ld find_magnitude();  //magnitude of the vector
-    Vector3D cross_product(const Vector3D &vec);    //cross_product
-    Vector3D normalization();   //normalized vector
-    Vector3D operator+(const Vector3D &vec)const;    //addition
-    Vector3D operator+(Vector &vec)const;
-    Vector3D &operator+=(const Vector3D &vec);  ////assigning new result to the vector
-    Vector3D operator-(const Vector3D &vec);    //substraction
-    Vector3D operator-(const _ld number)const;
-    Vector3D operator-()const;
-    Vector3D operator--();
-    Vector3D operator--(int);
-    Vector3D &operator-=(const Vector3D &vec);  //assigning new result to the vector
-    Vector3D operator*(_ld value);    //multiplication
-    Vector3D &operator*=(_ld value);  //assigning new result to the vector.
-    Vector3D operator/(_ld);    //division
+	_ld z, xAngle, yAngle, zAngle;
+	void set_allAngles();
+	void set_magnitude();
+	Vector3D check_division(_ld);
+	Vector3D* _ptr3d;
+	Vector3D(); //constructor
+	Vector3D(_ld, _ld, _ld);  //initializing object with values.
+	Vector3D(const Vector3D &vec);    //copy constructor
+	void setX(_ld);
+	void setY(_ld);
+	void setZ(_ld);
+	void set_xAngle(_ld);
+	void set_yAngle(_ld);
+	void set_zAngle(_ld);
+	void set_coordinates(_ld, _ld, _ld);
+	void showAllData();
+	void showPolarCord()const;
+	void display();    //display value of vectors
+	_ld returnX()const; //return x
+	_ld returnY()const; //return y
+	_ld returnZ()const; //return z
+	_ld return_xAngle()const; //return x angle
+	_ld return_yAngle()const; //return y angle
+	_ld return_zAngle()const; //return z angle
+	_ld square(); //gives square of the vector
+	_ld dot_product(const Vector3D &vec) const; //scalar dot_product
+	_ld distance(const Vector3D &vec);    //gives distance between two vectors
+	_ld find_magnitude();  //magnitude of the vector
+	Vector3D cross_product(const Vector3D &vec);    //cross_product
+	Vector3D normalization();   //normalized vector
+	Vector3D operator+(const Vector3D &vec)const;    //addition
+	Vector3D operator+(Vector &vec)const;
+	Vector3D &operator+=(const Vector3D &vec);  ////assigning new result to the vector
+	Vector3D operator-(const Vector3D &vec);    //substraction
+	Vector3D operator-(const _ld number)const;
+	Vector3D operator-()const;
+	Vector3D operator--();
+	Vector3D operator--(int);
+	Vector3D &operator-=(const Vector3D &vec);  //assigning new result to the vector
+	Vector3D operator*(_ld value);    //multiplication
+	Vector3D &operator*=(_ld value);  //assigning new result to the vector.
+	Vector3D operator/(_ld);    //division
 virtual Vector3D operator/(int);
-    Vector3D &operator/=(_ld value);  //assigning new result to the vector
-    Vector3D &operator=(const Vector3D &vec);
+	Vector3D &operator/=(_ld value);  //assigning new result to the vector
+	Vector3D &operator=(const Vector3D &vec);
 	friend ostream& operator<<(ostream&, const Vector3D&);
-    ~Vector3D();
+	~Vector3D();
 };
 #endif
 
 
+inline Vector3D::Vector3D() //constructor
+{
+  x=0;
+  y=0;
+  z=0;
+  xAngle = 0.0;
+  yAngle = 0.0;
+  zAngle = 0.0;
+  _ptr3d =  nullptr;
+ // _ptr2d = nullptr;
+  set_allAngles();
+  object_counter++;
+  //cout<< object_counter << ": " <<"in the 3dVector default constructor"<<endl;
+}
+inline Vector3D::Vector3D(_ld x1,_ld y1,_ld z1) //initializing object with values.
+{
+  x=x1;
+  y=y1;
+  z=z1;
+  xAngle = 0.0;
+  yAngle = 0.0;
+  zAngle = 0.0;
+  _ptr3d =  nullptr;
+ // _ptr2d = nullptr;
+  set_magnitude();
+  set_allAngles();
+  object_counter++;
+	//cout<< object_counter << ": " <<"in the x,y,z constructor" << endl;;
+}
+inline Vector3D::Vector3D(const Vector3D &vec)
+	: Vector(vec)
+{
+  x=vec.x;
+  y=vec.y;
+  z=vec.z;
+  xAngle = 0.0;
+  yAngle = 0.0;
+  zAngle = 0.0;
+  _ptr3d = nullptr;
+  set_magnitude();
+  set_allAngles();
+  object_counter++;
 
+}
+inline void Vector3D::setX(_ld _x)
+{
+	x = _x;
+	set_allAngles();
+	set_magnitude();
+}
+inline void Vector3D::setY(_ld _y)
+{
+	y = _y;
+	set_allAngles();
+	set_magnitude();
+}
+inline void Vector3D::setZ(_ld _z)
+{
+	z = _z;
+	set_allAngles();
+	set_magnitude();
+}
+inline void Vector3D::set_xAngle(_ld _xa)
+{
+	xAngle = _xa;
+	set_allAngles();
+	set_magnitude();
+}
+inline void Vector3D::set_yAngle(_ld _ya)
+{
+	yAngle = _ya;
+	set_allAngles();
+	set_magnitude();
+}
+inline void Vector3D::set_zAngle(_ld _za)
+{
+	zAngle = _za;
+	set_allAngles();
+	set_magnitude();
+}
+inline void Vector3D::set_coordinates(_ld x1, _ld y1, _ld z1)
+{
+	x = x1;
+	y = y1;
+	z = z1;
+	xAngle = 0.0;
+	yAngle = 0.0;
+	zAngle = 0.0;
+	_ptr3d = nullptr;
+	// _ptr2d = nullptr;
+	set_magnitude();
+	set_allAngles();
+	object_counter++;
+}
+inline void Vector3D::showAllData()
+{
+	magnitude = find_magnitude();
+	cout<< setprecision(9) << fixed;
+	cout<< "x: " << x << " ";
+	cout<< "y: " << y << " ";
+	cout<< "z: " << z << endl;
+	cout<< "mag: " << magnitude << endl;
+	cout << "angles aX: "<< xAngle << ", aY: "<<yAngle<<", aZ: "<<zAngle
+		 << endl;
+	show_mode();
+}
+inline void Vector3D::showPolarCord()const
+{
+	cout << setprecision(9) << fixed << "polar<" << x << ", " << y << ", " << z << ">";
+}
+inline void Vector3D::set_allAngles()
+{
+	xAngle = return_xAngle();
+	yAngle = return_yAngle();
+	zAngle = return_zAngle();
+	//angle =
+}
+
+inline void Vector3D::set_magnitude()
+{
+	magnitude = find_magnitude();
+}
+
+inline Vector3D Vector3D::check_division(_ld d)
+{
+	return *this;
+}
+
+//addition
+inline Vector3D Vector3D::operator+(const Vector3D &vec)const
+{
+	cout << "in Vector3D::operator+(const Vector3D &vec)"<<endl;
+	return Vector3D(x+vec.x,y+vec.y,z+vec.z);
+}
+inline Vector3D Vector3D::operator+(Vector &vec)const
+{
+	cout << "in Vector3D::operator+(Vector &vec)const "<<endl;
+	return Vector3D(x+vec.x,y+vec.y,z);
+}
+
+inline Vector3D &Vector3D::operator+=(const Vector3D &vec)
+{
+	cout << "in &Vector3D::operator+=(const Vector3D &vec)"<<endl;
+	x+=vec.x;
+	y+=vec.y;
+	z+=vec.z;
+	set_allAngles();
+	return *this;
+}
+
+//substraction//
+inline Vector3D Vector3D::operator-(const Vector3D &vec)
+{
+	return Vector3D(x-vec.x,y-vec.y,z-vec.z);
+}
+inline Vector3D Vector3D::operator-(const _ld n) const
+{
+	return Vector3D(x-n, y-n, z-n);
+}
+inline Vector3D Vector3D::operator-() const
+{
+	return Vector3D(-x, -y, -z);
+}
+inline Vector3D Vector3D::operator--()
+{
+	return Vector3D(--x, --y, --z);
+}
+inline Vector3D Vector3D::operator--(int)
+{
+	return Vector3D(x--, y--, z--);
+}
+inline Vector3D &Vector3D::operator-=(const Vector3D &vec)
+{
+	x-=vec.x;
+	y-=vec.y;
+	z-=vec.z;
+	set_allAngles();
+	return *this;
+}
+
+//scalar multiplication
+inline Vector3D Vector3D::operator*(_ld value)
+{
+	return Vector3D(x*value,y*value,z*value);
+}
+inline Vector3D &Vector3D::operator*=(_ld value)
+{
+	x*=value;
+	y*=value;
+	z*=value;
+	set_allAngles();
+	return *this;
+}
+
+//scalar division
+
+inline Vector3D Vector3D::operator/(_ld d)
+{
+	return check_division(d);
+
+}
+inline Vector3D Vector3D::operator/(int d)
+{
+	return check_division(d);
+
+}
+
+inline Vector3D &Vector3D::operator/=(_ld value)
+{
+
+	x/=value;
+	y/=value;
+	z/=value;
+	set_allAngles();
+	return *this;
+}
+
+inline Vector3D& Vector3D::operator=(const Vector3D& vec)
+{
+	this->_ptr3d = vec._ptr3d;
+	x = vec.x;
+	y = vec.y;
+	z = vec.z;
+	set_allAngles();
+
+	return *this;
+}
+
+inline ostream& operator<<(ostream& os, const Vector3D & v)
+{
+  if(v.mode == 'R' || v.mode == 'r'){
+	v.showRectCord();
+  }
+  else{
+	v.showPolarCord();
+  }
+  return os;
+}
+
+
+//Dot product
+inline _ld Vector3D::dot_product(const Vector3D &vec) const
+{
+	return x * vec.x + vec.y * y + vec.z * z;
+}
+//cross product
+inline Vector3D Vector3D::cross_product(const Vector3D &vec)
+{
+	_ld ni=y*vec.z-z*vec.y;
+	_ld nj=z*vec.x-x*vec.z;
+	_ld nk=x*vec.y-y*vec.x;
+	return Vector3D(ni,nj,nk);
+}
+inline _ld Vector3D::find_magnitude()
+{
+	return sqrt(square());
+}
+inline _ld Vector3D::square()
+{
+	return x*x+y*y+z*z;
+}
+
+inline Vector3D Vector3D::normalization()
+{
+	assert(find_magnitude()!=0);
+	*this/=find_magnitude();
+	return *this;
+}
+
+
+inline _ld Vector3D::distance(const Vector3D &vec)
+{
+	_ld x1, x2, y1, y2, z1, z2;
+	x1 = this->x;
+	x2 = vec.x;
+	y1 = this->y;
+	y2 = vec.y;
+	z1 = this->z;
+	z2 = vec.z;
+	return sqrt((pow(x2 - x1, 2) + pow(y2 - y1, 2) + pow(z2 - z1, 2)));
+}
+
+inline _ld Vector3D::returnX()const
+{
+	return x;
+}
+inline _ld Vector3D::returnY()const
+{
+	return y;
+}
+inline _ld Vector3D::returnZ()const
+{
+	return z;
+}
+inline _ld Vector3D::return_xAngle()const
+{
+	return atan2(sqrt(y * y + z * z), x) * DEGREE;
+}
+inline _ld Vector3D::return_yAngle()const
+{
+	return atan2(sqrt(x * x + z * z), y) * DEGREE;
+}
+inline _ld Vector3D::return_zAngle()const
+{
+	return atan2(sqrt(x * x  + y * y), z) * DEGREE;
+}
+inline void Vector3D::display()
+{
+	cout<<x<<" "<<y<<" "<<z<<endl;
+}
+
+inline Vector3D::~Vector3D()
+{
+	--object_counter;
+	delete _ptr3d;
+	//delete this;
+	//cout << "In Vector3D destructor: "<< object_counter << " objects remain\n"
+	//		 << endl;
+}
