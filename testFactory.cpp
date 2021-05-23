686f9b76b1158b99f29cb0cb8d98b973b8b779f3
Ryan Zurrin <rzurrin@gmail.com>
2021-05-22 23:50:27 -0400

    bed time save

--- testFactory.cpp
+++ testFactory.cpp
@@ -13,33 +13,10 @@ int main()
 	//add test code between starred areas
 	//*************************************************************************
 
-	//setVal(QP::timeToTravelDistance(420e-9, _ELECTRON_MASS_, 2.71, 2.50e-2));
-	//show_val("wl", "nm");
-	Vector3D bb(4, 6, -3);
-	Vector3D a(2, -4, 1, "helpme");
-	a.displayAllData();
-	a.normalize_vector();
-	a.displayAllData("a normal:");
-	Vector3D b(-1, 4, 7);
-	Vector3D c = a.cross_product(b);
-	c.displayAllData("c = axb:");
-	Vector3D d = c.cross_product(bb);
+	setVal(QP::powerCarriedAwayByElectrons(1.00e-3, 500e-9, 1.30e3,2.28));
+	show_val("N/t", "elec/sec");
+	
 
-	std::cout << "mag: "<< c.return_mag()<< std::endl;
-	std::cout << "dot: "<< a.dot_product(b)<< std::endl;
-	std::cout << "angle between: "<< a.angle_between_vectors(b)<< std::endl;
-	std::cout << "angle between: "<< b.angle_between_vectors(a)<< std::endl;
-
-
-	Vector2D a1(1, 7);
-	Vector2D b1(15, 3);
-	std::cout << "2d Dot prod: "<< a1.dot_product(b1)<< std::endl;
-	std::cout << "projection: "<< a1.projection(b1)<< std::endl;
-	std::cout << "b*proj: "<< a1.projection(b1)* b1.return_mag()<< std::endl;
-
-
-	TriangleSolver triangle(5, 3, 7);
-	std::cout << triangle;
 	//*************************************************************************
 	//Stopping timer and displaying the programs execution time
 	//_________________________________________________________________________
