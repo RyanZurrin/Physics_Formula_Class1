42e35986ebd9acdc448c3ceeaf5da9d0f3adb615
Ryan Zurrin <rzurrin@gmail.com>
2021-10-03 21:36:51 -0400

    added armadillo

--- testFactory.cpp
+++ testFactory.cpp
@@ -15,31 +15,24 @@ int main()
 	//*************************************************************************
 
 
-	double A[3][4] = {
-		{1.0, 1.0, -1.0, 2.0},
-		{3.0, 1.0, 4.0, 11.0},
-		{2.0, 3.0, -5.0, 2.0}
-	};
-	to_reduced_row_echelon_form(A);
+	arma::arma_rng::set_seed_random();
 
-	print_matrix(A, 3, 4);
-	Vector3D t(-1, 2, 1);
-	std::cout << "distance: " << t.distance_to_plane(1, 2, -2, -13) << endl;
+	arma::Mat<double> A = arma::randu(4, 4);
+	std::cout << "A:\n" << A << endl;
 
-	Vector2D g(2, 4);
-	std::cout << "distance: " << g.distance_to_line(1, 2, -3) << endl;
+	std::cout << "A * A.t() =\n";
+	std::cout << A * A.t() << endl;
 
-	Eigen::Matrix<double, 3, 3> det;
-	det << 1, 2, 4, 2, -1, 3, 4, 0, 1;
-	std::cout << det << endl;
-	std::cout << "\ndeterminant: " << det.determinant() << endl;;
+	A.row(0) = A.row(1) + A.row(3);
+	A.col(3).zeros();
+	std::cout << "add rows 1 and 3, store result in row 0, also fill 4th column with zeros:\n";
+	std::cout << "A:\n" << A << "\n";
 
-	Eigen::Matrix<double, 4, 4> dett;
-	dett << 1, 2, 3, 4, 1, 0, 2, 0, 0, 1, 2, 3, 2, 3, 0, 0;
-	std::cout << dett << endl;
-	std::cout << "\ndeterminant: " << det.determinant() << endl;
+	arma::Mat<double>B = arma::diagmat(A);
+	std::cout << "B:\n" << B << "\n";
 
-	std::cout << "\ninverse:\n" << det.inverse() << endl;
+	A.save("A_mat.txt", arma::arma_ascii);
+	B.save("B_mat.txt", arma::arma_ascii);
 
 
 
